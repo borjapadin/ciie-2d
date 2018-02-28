753914a1f5038092a6cddf2a21f5837196f15ac3
jessicapfernandez <jessica.pfernandez96@outlook.es>
2018-02-28 16:47:30 +0000

    pruebas

--- juego2D/main.py
+++ juego2D/main.py
@@ -1,9 +1,12 @@
 #Importacion modulos
-import pygame
+import pygame, sys
+from pygame.locals import *
 
 #Constantes
 ANCHO_PANTALLA = 800
 ALTO_PANTALLA = 600
+
+BLANCO  = (255,255,255)
 
 if __name__ == '__main__':
 
@@ -12,3 +15,13 @@ if __name__ == '__main__':
 
 	#Crear la pantalla
 	pantalla = pygame.display.set_mode((ANCHO_PANTALLA,ALTO_PANTALLA),0,32)
+
+	#Rellenamos pantalla color negro
+	pantalla.fill((0,0,0))
+
+	pygame.draw.circle(pantalla, BLANCO, (50,50),4,0)
+
+	pygame.display.update()
+
+	pygame.quit()
+	sys.exit()
