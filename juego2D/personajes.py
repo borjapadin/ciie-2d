e24f03bbb5725d73b8b393d2fc20043277e945e9
borjapadin <borja2padin@gmail.com>
2018-03-01 20:55:10 +0100

    escena y personaje estructura

---
+++ juego2D/personajes.py
@@ -0,0 +1,36 @@
+
+import pygame, sys, os
+from pygame.locals import *
+from escena import *
+from gestorRecursos import *
+
+# Constantes--------------
+GRAVEDAD = 0.0003 # Píxeles / ms2
+# Movimientos
+
+#Posturas
+
+# Velocidades protagonista
+VELOCIDAD_JUGADOR = 0.2 # Pixeles por milisegundo
+VELOCIDAD_SALTO_JUGADOR = 0.3 # Pixeles por milisegundo
+RETARDO_ANIMACION_JUGADOR = 5 # updates que durará cada imagen del personaje
+                              # debería de ser un valor distinto para cada postura
+# velocidades de enemigos
+
+#----------------------------
+# Clase MiSprite
+
+#---------------------------
+# Clases Personaje
+
+#---------------------------
+# Clase Jugador
+
+#---------------------------
+# Clase Zombi
+
+#---------------------------
+# Clase Soldado
+
+#---------------------------
+# Clase Jefe
