6ae4bb8e8d88ea6c07ecc543da589d4401656b1e
esther-sanchezm <esther-sanchezm@users.noreply.github.com>
2018-03-05 16:22:32 +0100

    Update menu.py

--- juego2D/menu.py
+++ juego2D/menu.py
@@ -111,3 +111,52 @@ class PantallaGUI:
 		#Después los botones
 		for elemento in self.elementoGUI:
 			elemento.dibujar(pantalla)
+# -------------------------------------------------
+# Clase Menu, la escena en sí
+class Menu(Escena):
+
+    def __init__(self, director):
+        # Llamamos al constructor de la clase padre
+        Escena.__init__(self, director);
+        # Creamos la lista de pantallas
+        self.listaPantallas = []
+        # Creamos las pantallas que vamos a tener
+        #   y las metemos en la lista
+        self.listaPantallas.append(PantallaInicialGUI(self))
+        # En que pantalla estamos actualmente
+        self.mostrarPantallaInicial()
+
+    def update(self, *args):
+        return
+
+    def eventos(self, lista_eventos):
+        # Se mira si se quiere salir de esta escena
+        for evento in lista_eventos:
+            # Si se quiere salir, se le indica al director
+            if evento.type == KEYDOWN:
+                if evento.key == K_ESCAPE:
+                    self.salirPrograma()
+            elif evento.type == pygame.QUIT:
+                self.director.salirPrograma()
+
+        # Se pasa la lista de eventos a la pantalla actual
+        self.listaPantallas[self.pantallaActual].eventos(lista_eventos)
+
+    def dibujar(self, pantalla):
+        self.listaPantallas[self.pantallaActual].dibujar(pantalla)
+
+    #--------------------------------------
+    # Metodos propios del menu
+
+    def salirPrograma(self):
+        self.director.salirPrograma()
+
+    def ejecutarJuego(self):
+        fase = Fase(self.director)
+        self.director.apilarEscena(fase)
+
+    def mostrarPantallaInicial(self):
+        self.pantallaActual = 0
+
+    # def mostrarPantallaConfiguracion(self):
+    #    self.pantallaActual = ...
