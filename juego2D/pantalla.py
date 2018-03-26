a04dccaf0cd60a086e9af288f9e61f8d010bbb07
uxiapastoriza <uxia.pastoriza@udc.es>
2018-03-26 19:04:27 +0200

    Revert "pantalla"
    This reverts commit f14f382cedb53e3f70b012addc55bdb3a71178c6.

--- juego2D/pantalla.py
+++ juego2D/pantalla.py
@@ -12,111 +12,67 @@ from boton import *
 from constantes import *
 
 class Pantalla(PantallaGUI):
-	def __init__(self, menu, imagen):
-	        PantallaGUI.__init__(self, menu, imagen)
+    def __init__(self, menu, imagen):
+        PantallaGUI.__init__(self, menu, imagen)
         
-	         # Creamos el texto y lo metemos en la lista
-		if imagen == 'Menu/CutScene/PantallaSiguienteNivel.png':
-			textoContinuar = TextoContinuar(self)
-	                self.elementosGUI.append(textoContinuar)
-	                textoSalir = TextoSalir(self)
-	                self.elementosGUI.append(textoSalir) 
-	                
-	                self.botonContinuar = BotonContinuar(self)   
-	                self.botonCuteSceneSalir = BotonSalir(self)   
-	                        
-	                self.addBotonContinuar()
+         # Creamos el texto y lo metemos en la lista
+        textoSalir = TextoSalir(self)
+        self.elementosGUI.append(textoSalir) 
+        self.botonSalir = BotonSalir(self)   
+
+        if imagen == 'Menu/CutScene/PantallaSiguienteNivel.png':
+            textoContinuar = TextoContinuar(self)
+            self.elementosGUI.append(textoContinuar)
+            self.botonContinuar = BotonContinuar(self)
+            self.addBotonContinuar()
      
-	        elif imagen == 'Menu/Inicio/PantallaInicio.jpg':
-			textoJugar = TextoJugar(self)
-	                textoSalir = TextoSalir(self)
-	    
-	                self.elementosGUI.append(textoJugar) 
-	                self.elementosGUI.append(textoSalir) 
-	    
-	                self.botonJugar = BotonJugar(self)
-	                self.botonSalirPantallaIni = BotonSalir(self)
-    
-	                self.addBotonJugar()
-	            
-	        else:
-	                textoVolverJuego = TextoVolverJuego(self) 
-	                textoSalir = TextoSalir(self)
-    
-	                self.elementosGUI.append(textoVolverJuego)
-	                self.elementosGUI.append(textoSalir)
-    
-         
-	                #Creamos los botones
-	                self.botonSalir = BotonSalir(self) 
-	                self.botonVolverJuego = BotonVolverJuego(self)
-    
-	                #Agregamos solo el que va a estar seleccionado la primera vez que se cree
-	                self.addBotonVolverJugar()
+        elif imagen == 'Menu/Inicio/PantallaInicio.jpg':
+            textoJugar = TextoJugar(self)
+            self.elementosGUI.append(textoJugar) 
+            self.botonJugar = BotonJugar(self)
+            self.addBotonJugar()
+        else:
+            textoVolverJuego = TextoVolverJuego(self)
+            self.elementosGUI.append(textoVolverJuego)
+            self.botonVolverJuego = BotonVolverJuego(self)
+            self.addBotonVolverJuego()
     
     
-    
-	def addBotonContinuar(self):
-	        self.elementosGUI.append(self.botonContinuar)
-        	self.eventoSeleccionado = "continuar"            
+    def addBotonContinuar(self):
+        self.elementosGUI.append(self.botonContinuar)
+        self.eventoSeleccionado = "continuar"        
         
-    	def addBotonSalir(self):
-        	self.elementosGUI.append(self.botonSalir)
-        	self.eventoSeleccionado = "salir"   
+    def addBotonSalir(self):
+        self.elementosGUI.append(self.botonSalir)
+        self.eventoSeleccionado = "salir"   
 
-    	def addBotonJugar(self):
-       		self.elementosGUI.append(self.botonJugar)
-        	self.eventoSeleccionado = "jugar"        
     
-    	def addBotonVolverJugar(self):
-        	self.elementosGUI.append(self.botonVolverJuego)
-        	self.eventoSeleccionado = "volverJugar" 
+    def addBotonJugar(self):
+        self.elementosGUI.append(self.botonJugar)
+        self.eventoSeleccionado = "jugar"        
+    
+    def addBotonVolverJuego(self):
+        self.elementosGUI.append(self.botonVolverJuego)
+        self.eventoSeleccionado = "volverJugar" 
 
-    	def addBotonSalirCuteScene(self):
-        	self.elementosGUI.append(self.botonCuteSceneSalir)
-        	self.eventoSeleccionado = "salirCuteScene" 
-
-        def addbBotonSalirPantallaIni(self):
-            self.elementosGUI.append(self.botonSalirPantallaIni)
-            self.eventoSeleccionado = "salirPantallaIni" 
-
-
-    	#Sobreescribir
-    	def eventos(self, lista_eventos):
-        	for evento in lista_eventos:
-            		if evento.type == KEYDOWN: 
-                		#Aceptar acción
-                		if evento.key == K_RETURN:    
-                    			elemento = self.elementosGUI.pop()
-                    			elemento.accion()
-                    			self.elementosGUI.append(elemento)
+    #Sobreescribir
+    def eventos(self, lista_eventos):
+        for evento in lista_eventos:
+            if evento.type == KEYDOWN: 
+                #Aceptar acción
+                if evento.key == K_RETURN: 
+                    elemento = self.elementosGUI.pop()
+                    elemento.accion()
+                    self.elementosGUI.append(elemento)
                           
-                    		#Cambiar de opción
-                		if evento.key == K_DOWN or evento.key == K_UP:
-                    			if self.eventoSeleccionado == "volverJugar":
-                        			self.elementosGUI.pop()
-                        			self.addBotonSalir()
+                #Cambiar de opción
+                if evento.key == K_DOWN or evento.key == K_UP:
+                    self.elementosGUI.pop()
+                    if self.eventoSeleccionado == "jugar":
+                        if imagen == 'Menu/CutScene/PantallaSiguienteNivel.png' or imagen == 'Menu/Inicio/PantallaInicio.jpg':
+                        	self.addBotonContinuar() 
+                    else:
+                        self.addBotonSalir()
                         
-                    			elif self.eventoSeleccionado == "salirPantallaIni":  
-                        			self.elementosGUI.pop()
-                        			self.addBotonJugar()
-
-                    			elif self.eventoSeleccionado == "jugar":
-                        			self.elementosGUI.pop()
-                        			self.addbBotonSalirPantallaIni()
-
-                    			elif self.eventoSeleccionado == "salirCuteScene":
-                        			self.elementosGUI.pop()
-                        			self.addBotonContinuar() 
-
-                    			elif self.eventoSeleccionado == "continuar":
-                        			self.elementosGUI.pop()
-                       				self.addBotonSalirCuteScene()
-
-                                	else:
-                                    		self.elementosGUI.pop()
-                                    		self.addBotonVolverJugar()
-
-                        
-                		if evento.type == pygame.QUIT: 
-                    			self.director.salirPrograma()                     
+                if evento.type == pygame.QUIT: 
+                    self.director.salirPrograma()                     
